f0dfcada50efc6913c3f7f219f16d10a18dd154e
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2018-02-18 01:59:41 +0800

    Main menu and load out scenes, scene transitions

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	#region main menu (MM)
+	public void MMPlayPressed()
+	{
+		SceneManager.LoadScene("loadout",LoadSceneMode.Single);
+	}
+	#endregion
+
+	#region loadout menu (LM)
+	public void LMConfirmPressed()
+	{
+		SceneManager.LoadScene("scene",LoadSceneMode.Single);
+	}
+
+	#endregion
+}
